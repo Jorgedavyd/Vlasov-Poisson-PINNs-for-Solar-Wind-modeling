8a05f41c17b35e14cdb2aa33273adcc5e5bc5656
Jorgedavyd <jorged.encyso@gmail.com>
2024-11-11 11:12:11 -0300

    default

---
+++ model.py
@@ -0,0 +1,23 @@
+from lightorch.training.supervised import Module
+from torch import nn, Tensor
+from .loss import criterion
+
+
+class model_name(nn.Module):
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return out
+
+
+class Model(Module):
+    def __init__(self, **hparams) -> None:
+        super().__init__(**hparams)
+        self.model = model_name()
+        self.criterion = criterion()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.model(x)
