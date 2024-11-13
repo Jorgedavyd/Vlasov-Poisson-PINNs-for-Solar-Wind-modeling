e1a9d319438edfefe778f99251f518f03ee8313d
Jorgedavyd <jorged.encyso@gmail.com>
2024-11-13 13:01:11 -0300

    checked blacked

--- download_data.py
+++ download_data.py
@@ -4,9 +4,9 @@ from utils import create_dates
 if __name__ == "__main__":
     DataDownloading(
         [
-            WIND.TDP_PM(root="/data/Vlasov"),
-            # DSCOVR.FaradayCup(root = '/data/Vlasov'),
-            # DSCOVR.Magnetometer(root =  '/data/Vlasov'),
+            WIND.TDP_PM(root="/data/Vlasov/TDP_PM"),
+            DSCOVR.FaradayCup(root="/data/Vlasov/DSCOVR"),
+            DSCOVR.Magnetometer(root="/data/Vlasov/DSCOVR"),
         ],
         create_dates(),
     )
