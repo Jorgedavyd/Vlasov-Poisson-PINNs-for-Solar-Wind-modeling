c69f4b1f52b78cf259563af90078b2c90cf3a14e
Jorgedavyd <jorged.encyso@gmail.com>
2024-11-14 12:37:12 -0300

    default

--- download_data.py
+++ download_data.py
@@ -1,12 +1,21 @@
-from starstream import DataDownloading, WIND, DSCOVR
+from starstream import DataDownloading, WIND, DSCOVR, ACE
 from utils import create_dates
 
 if __name__ == "__main__":
-    DataDownloading(
-        [
-            WIND.TDP_PM(root="/data/Vlasov/TDP_PM"),
-            DSCOVR.FaradayCup(root="/data/Vlasov/DSCOVR"),
-            DSCOVR.Magnetometer(root="/data/Vlasov/DSCOVR"),
-        ],
-        create_dates(),
-    )
+    for tuple_date in create_dates():
+        try:
+            DataDownloading(
+                [
+                    WIND.TDP_PM(root="/data/Vlasov/TDP_PM"),
+                    DSCOVR.FaradayCup(root="/data/Vlasov/DSCOVR/FaradayCup"),
+                    DSCOVR.Magnetometer(root="/data/Vlasov/DSCOVR/Magnetometer"),
+                    ACE.SWEPAM(download_path="/data/Vlasov/ACE/SWEPAM"),
+                    ACE.SWICS(download_path="/data/Vlasov/ACE/SWICS"),
+                    ACE.EPAM(download_path="/data/Vlasov/ACE/EPAM"),
+                    ACE.MAG(download_path="/data/Vlasov/ACE/MAG"),
+                    ACE.SIS(download_path="/data/Vlasov/ACE/SIS"),
+                ],
+                tuple_date,
+            )
+        except:
+            continue
