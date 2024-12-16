c1dc957a0c9fe1375e87d81417e6cdb710ca4a5a
Jorgedavyd <jorged.encyso@gmail.com>
2024-12-16 10:32:15 -0300

    default

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+from modulus.sym.domain.constraint.discrete import DictGridDataset
 from modulus.sym.hydra import ModulusConfig
 from modulus.sym.solver import Solver
 from modulus.sym.domain import Domain
+from numpy._typing import NDArray
+from scipy.interpolate import CubicSpline
 from sympy import Or
 from argparse import ArgumentParser
-from typing import List, Callable
+from typing import List, Callable, Tuple
 import modulus.sym
 from modulus.sym import Node
 from modulus.sym.domain.constraint import (
@@ -22,9 +25,10 @@ from equations import (
     define_velocity_field,
 )
 from model import NeuralNetwork, FNOModel
-
+import numpy as np
 from modulus.sym.geometry.primitives_3d import Geometry, Sphere
 from modulus.sym.utils.io.vtk import var_to_polyvtk
+import pandas as pd
 
 
 def make_geometry(cfg: ModulusConfig):
@@ -89,12 +93,63 @@ def define_pde_constraints(
     return domain
 
 
+def get_interpolators(variables: List[str], path: str) -> Tuple[CubicSpline, ...]:
+    interpolators: List[CubicSpline] = list()
+    data: pd.DataFrame = pd.read_csv(path)
+    time: NDArray = data.index.values.astype(np.float32)
+    for variable in variables:
+        interpolators.append(CubicSpline(time, data[variable].values))
+    return interpolators
+
+
 def l1_boundary(nodes: List[Node], domain: Domain, cfg: ModulusConfig) -> Domain:
-    train_dataset = DictGridDataset()  ## mirar como parametrizar
+    path: str = "/data/Vlasov/curated.csv"
+    variables: List[str] = [
+        "E_x",
+        "E_y",
+        "E_z",
+        "B_x",
+        "B_y",
+        "B_z",
+        "V_x",
+        "V_y",
+        "V_z",
+    ]
+    time_grid: NDArray = np.arange(0, 1, cfg.geometry.grid_resolution)
+    (
+        E_x_interpolator,
+        E_y_interpolator,
+        E_z_interpolator,
+        B_x_interpolator,
+        B_y_interpolator,
+        B_z_interpolator,
+        V_x_interpolator,
+        V_y_interpolator,
+        V_z_interpolator,
+    ) = get_interpolators(variables, path)
+    train_dataset = DictGridDataset(
+        invar=dict(
+            x=np.zeros_like(time_grid),
+            y=np.zeros_like(time_grid),
+            z=np.zeros_like(time_grid),
+            t=time_grid,
+        ),
+        outvar=dict(
+            E_x=E_x_interpolator.interpolate(time_grid),
+            E_y=E_y_interpolator.interpolate(time_grid),
+            E_z=E_z_interpolator.interpolate(time_grid),
+            B_x=B_x_interpolator.interpolate(time_grid),
+            B_y=B_y_interpolator.interpolate(time_grid),
+            B_z=B_z_interpolator.interpolate(time_grid),
+            V_x=V_x_interpolator.interpolate(time_grid),
+            V_y=V_y_interpolator.interpolate(time_grid),
+            V_z=V_z_interpolator.interpolate(time_grid),
+        ),
+    )
     l1_parameters = SupervisedGridConstraint(
         nodes=nodes,
         dataset=train_dataset,
-        batch_size=cfg.batch_size.boundary1,
+        batch_size=cfg.batch_size.boundary,
     )
     domain.add_constraint(l1_parameters, "l1_bc")
     return domain
